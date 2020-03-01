6fcfb1146328f3a709fffba878ab16c22b940fe8
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-03-01 14:24:26 +0100

    test

--- src/main/java/pl/allegro/Test.java
+++ src/main/java/pl/allegro/Test.java
@@ -1,4 +1,11 @@
 package pl.allegro;
 
+import org.openqa.selenium.json.JsonOutput;
+
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("asdsad");
+    }
+
 }

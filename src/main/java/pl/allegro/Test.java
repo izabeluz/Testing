330f8f5f551192cd55ed7ea5aa6d6bd9001c99b5
Pawel Pawlak (ext.) <Pawel.Pawlak@partner.bmw.de>
2020-04-16 21:00:37 +0200

    test data

--- src/main/java/pl/allegro/Test.java
+++ src/main/java/pl/allegro/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Testing v1");
+        System.out.println("Nowe 1");
         System.out.println("asdsad");
+        System.out.println("Nowe 2");
     }
 }

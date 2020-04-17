2471bce912b750923c158daa056875c58b7330ec
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-04-17 13:32:25 +0200

    Test2

---
+++ src/main/java/pl/allegro/Test2.java
@@ -0,0 +1,12 @@
+package pl.allegro;
+
+public class Test2 {
+    public static void main(String[] args) {
+        for (int i = 13; i <= 100; i++) {
+            if (i % 7 == 0) {
+                System.out.print(i + ",");
+            }
+
+        }
+    }
+}

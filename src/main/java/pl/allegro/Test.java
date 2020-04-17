5478181c329310b6f3133972e4777fd448cb72b0
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-04-17 10:07:19 +0200

    Test- change

--- src/main/java/pl/allegro/Test.java
+++ src/main/java/pl/allegro/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Testing v1");
-        System.out.println("asdsad");
+        System.out.println("Nowe 4");
+        System.out.println("Nowe 3");
+        System.out.println("Nowe 5");
     }
 }

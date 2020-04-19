97d5fcec52f02a5a38841d788cdafae71405b5c0
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-04-19 16:23:59 +0200

    Test2 change

--- src/main/java/pl/allegro/Test2.java
+++ src/main/java/pl/allegro/Test2.java
@@ -2,11 +2,16 @@ package pl.allegro;
 
 public class Test2 {
     public static void main(String[] args) {
-        for (int i = 13; i <= 100; i++) {
+        System.out.print("Liczby podzielne przez 7 z zakresu od 13 do 170 to: ");
+        for (int i = 13; i <= 170; i++) {
             if (i % 7 == 0) {
                 System.out.print(i + ",");
             }
-
         }
+        System.out.println("");
+        System.out.println("Ilość liczb podzielnych przez 7 to :");
+
+
     }
 }
+

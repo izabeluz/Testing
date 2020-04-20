309cf4d8c52e36dcd247523212931ba072e112a5
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-04-20 17:24:42 +0200

    TEST2

--- src/main/java/pl/allegro/Test2.java
+++ src/main/java/pl/allegro/Test2.java
@@ -20,7 +20,7 @@ public class Test2 {
     System.out.print("Liczby podzielne przez 7 z zakresu od 13 do 170 to :");
     licznik = 0;
     for (int i = 13; i <= 170; i++) {
-      if (i % 7 == 0) {// podzilene przez 7
+      if (i % 7 == 0) {// podzielne przez 7
         if (licznik % 5 == 0) {//co 5 element ma byc nowa linia
           System.out.println(""); //nowa linia
         }

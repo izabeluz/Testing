ef753274ebb73c12b70375c7647bcc71e1e3b115
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-04-22 16:26:07 +0200

    TESTAllegro

--- src/test/java/pl/pawlak/allegro/AllegroTest.java
+++ src/test/java/pl/pawlak/allegro/AllegroTest.java
@@ -5,7 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"File1.feature", "File2.feature"}
+@CucumberOptions(features = "src/BDD/Features/File1.feature, File2.feature",
         plugin = {"pretty", "html:out"})
-class AllegroTest {
+public class AllegroTest {
+
 }

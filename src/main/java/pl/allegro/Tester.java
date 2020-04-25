fcd9a6d4cbae085ee9adf51c50d182a8f4d19249
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-04-25 18:03:20 +0200

    Tester

--- src/main/java/pl/allegro/Tester.java
+++ src/main/java/pl/allegro/Tester.java
@@ -2,6 +2,18 @@ package pl.allegro;
 
 public class Tester {
   public static void main(String[] args) {
-    Exercise myObject = new Exercise();
+    Exercise exercise1 = new Exercise();//jeden objekt
+//    exercise1.calculateNumber();//wywolanei metody na obiekcie zwykle
+//    exercise1.getMaxNumber();
+//    Exercise.getMaxValue();//static na klasie wywołana- statyczne
+//    exercise1.increaseNumber();//
+    int suma1 = exercise1.sum(10, 20);
+    int suma2 = exercise1.sum(30, 50);
+//    System.out.println(exercise1.sum(suma1, suma2));
+//    System.out.println(exercise1.test());
+    // napisz mi liczbu podzielne przez 13 od 20 do 3000
+    exercise1.increaseNumber(13, 20, 3000);//
+
+
   }
 }

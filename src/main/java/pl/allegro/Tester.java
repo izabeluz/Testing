986c9083f5f65a2beba0ecc856227d1a9b454894
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-05-01 20:29:25 +0200

    Tester

--- src/main/java/pl/allegro/Tester.java
+++ src/main/java/pl/allegro/Tester.java
@@ -17,7 +17,7 @@ public class Tester {
     //exercise1.increaseNumber(13, 20, 3000);//
 //    double calculation = exercise1.getTriangleArea(10, 15);
 //    System.out.println(calculation);
-//    System.out.println(exercise1.getTriangleObwod(5, 10, 15));
+//    System.out.println(exercise1.getTriangleCircuit(5, 10, 15));
     int tab[] = new int[] {2, 6, 8, 9, 21, 21, 20, 69, 58};
     exercise1.getMaximumNumber(tab);
     System.out.println(exercise1.getMaximumNumber(tab));
@@ -25,6 +25,9 @@ public class Tester {
     System.out.println(exercise1.getMinimumNumber(tab));
     exercise1.getAverageNumber(tab);
     System.out.println(exercise1.getAverageNumber(tab));
+    exercise1.getCalculateNumbers(7, 20, 55);
+    exercise1.getCalculateCostOfTravel(372, 5.5, 100, 3.79);
+    System.out.println(exercise1.getCalculateCostOfTravel(372, 5.5, 100, 3.79));
 
 
   }

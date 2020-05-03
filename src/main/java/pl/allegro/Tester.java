86181f9d1944cdc43f68d22d03534fffde2c218e
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-05-03 12:23:03 +0200

    Tester correct

--- src/main/java/pl/allegro/Tester.java
+++ src/main/java/pl/allegro/Tester.java
@@ -4,20 +4,20 @@ import java.awt.*;
 
 public class Tester {
   public static void main(String[] args) {
-    Exercise exercise1 = new Exercise();//jeden objekt
-//    exercise1.calculateNumber();//wywolanei metody na obiekcie zwykle
-//    exercise1.getMaxNumber();
-//    Exercise.getMaxValue();//static na klasie wywołana- statyczne
-//    exercise1.increaseNumber();//
-//    int suma1 = exercise1.sum(10, 20);
-//    int suma2 = exercise1.sum(30, 50);
-//    System.out.println(exercise1.sum(suma1, suma2));
-//    System.out.println(exercise1.test());
-    // napisz mi liczbu podzielne przez 13 od 20 do 3000
+    Exercise exercise1 = new Exercise();//one object
+    exercise1.calculateNumber();//calling the  method on the object
+    exercise1.getMaxNumber();
+    Exercise.getMaxValue();//calling static method ont the class
+    exercise1.increaseNumber(13,20,3000);//
+    int suma1 = exercise1.sum(10, 20);
+    int suma2 = exercise1.sum(30, 50);
+    System.out.println(exercise1.sum(suma1, suma2));
+    System.out.println(exercise1.sum(5,20));
+
     //exercise1.increaseNumber(13, 20, 3000);//
-//    double calculation = exercise1.getTriangleArea(10, 15);
-//    System.out.println(calculation);
-//    System.out.println(exercise1.getTriangleCircuit(5, 10, 15));
+    double calculation = exercise1.getTriangleArea(10, 15);
+    System.out.println(calculation);
+    System.out.println(exercise1.getTriangleCircut(5, 10, 15));
     int tab[] = new int[] {2, 6, 8, 9, 21, 21, 20, 69, 58};
     exercise1.getMaximumNumber(tab);
     System.out.println(exercise1.getMaximumNumber(tab));
